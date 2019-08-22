b27ac6b9e6e82d90241285ed3cb19e09f55d931a
sksvns <sksvnsss@gmail.com>
2019-08-22 16:40:04 +0530

    redux introduced with header component

--- src/components/header/header.component.js
+++ src/components/header/header.component.js
@@ -1,6 +1,7 @@
 
 import React from 'react';
 import {Link} from 'react-router-dom';
+import {connect} from 'react-redux';
 
 import './header.scss';
 
@@ -32,6 +33,10 @@ const Header = ({currentUser}) => (
 	        ) }
 	  </div>
 	</div>
-)
+);
 
-export default Header;
+const mapStateToProps = state => ({
+	currentUser: state.user.currentUser
+})
+
+export default connect(mapStateToProps)(Header);
